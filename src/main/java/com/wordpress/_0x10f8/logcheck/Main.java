8b2ac17762d91d9678054f6042ba29145e6e9aad
c0g <cgardi203@caledonian.ac.uk>
2018-11-06 22:23:52 +0000

    GUI Removal and Improved Output Test
    Removed the GUI as would prefer CLi tool and the GUI wasn't good. Improved the Main class output.

--- src/main/java/com/wordpress/_0x10f8/logcheck/Main.java
+++ src/main/java/com/wordpress/_0x10f8/logcheck/Main.java
@@ -1,10 +1,14 @@
 package com.wordpress._0x10f8.logcheck;
 
+import com.wordpress._0x10f8.logcheck.log.LogEntryReference;
 import com.wordpress._0x10f8.logcheck.match.RuleMatch;
 import com.wordpress._0x10f8.logcheck.rule.Rule;
 import com.wordpress._0x10f8.logcheck.rule.RuleFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,9 +17,12 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        final List<Rule> rules = RuleFactory.loadRulesFromFiles(Arrays.asList(new File("./rules/SQLiCompositeRule.json"),
-                new File("./rules/VeryActiveClient.json"), new File("./rules/XSSCompositeRule.json")));
-        final File logFile = new File("./logs/test_log");
+        final List<Rule> rules = RuleFactory.loadRulesFromFiles(Arrays.asList(
+                new File("./rules/SQLiCompositeRule.json"),
+                new File("./rules/VeryActiveClient.json"),
+                new File("./rules/XSSCompositeRule.json")));
+
+        final File logFile = new File("./logs/test_log2");
 
         final List<RuleMatch> allMatches = new ArrayList<>();
 
@@ -28,8 +35,22 @@ public class Main {
 
         for (int i = 0; i < allMatches.size() && i < 100; i++) {
             final RuleMatch match = allMatches.get(i);
-            System.out.println("Found match " + match.getMatchingRuleName() + " " + match.getDescription());
+
+            final String ruleName = match.getMatchingRuleName();
+            final String description = match.getDescription();
+            final LogEntryReference reference = match.getLogReference();
+            final String logLine = getLogLine(reference.getLogFile(), reference.getLogLine());
+
+            System.out.println("Found match " + ruleName + " " + (description == null ? "" : description) +
+                    " in log file [" + reference.getLogFile().getAbsolutePath() + "] line: " + reference.getLogLine());
+            System.out.println("\t " + logLine);
+
         }
 
+    }
+
+    private static String getLogLine(final File logFile, final int line) throws IOException {
+        final List<String> lines = Files.readAllLines(Paths.get(logFile.toURI()));
+        return lines.get(line - 1);
     }
 }
